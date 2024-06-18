9e9cb3a0cdc158a82d4e379df90c32762da37f43
BOYER AXEL <axel.boyer2@etu.unistra.fr>
2024-06-18 21:40:45 +0200

    Initiliazed UI with Navbar & created session recorder component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MyBoardComponent } from './my-board/my-board.component';
+import { SessionRecorderComponent } from './session-recorder/session-recorder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: MyBoardComponent },
+  { path: 'sessionRecorder', component: SessionRecorderComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home as default
+];
---
+++ src/app/my-board/my-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-board.component.html',
+  styleUrl: './my-board.component.css'
+})
+export class MyBoardComponent {
+
+}
